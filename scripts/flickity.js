85b8b2200e898f005dd283b3eef61f9f9e9627d5
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2020-07-31 16:50:16 +0800

    feat: add config for flickity carousel

---
+++ scripts/flickity.js
@@ -0,0 +1,32 @@
+var utils = window.fizzyUIUtils;
+
+var carousel = document.querySelector('.carousel-projects');
+var flkty = new Flickity( carousel, {
+  prevNextButtons: false,
+  pageDots: false
+});
+
+// previous
+var previousButton = document.querySelector('.carousel-prev');
+previousButton.addEventListener( 'click', function() {
+  flkty.previous();
+});
+
+// next
+var nextButton = document.querySelector('.carousel-next');
+nextButton.addEventListener( 'click', function() {
+  flkty.next();
+});
+
+var imgs = carousel.querySelectorAll('.carousel-cell img');
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+  'transform' : 'WebkitTransform';
+
+// flkty.on( 'scroll', function() {
+//   flkty.slides.forEach( function( slide, i ) {
+//     var img = imgs[i];
+//     var x = ( slide.target + flkty.x ) * -1/3;
+//     img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+//   });
+// });
